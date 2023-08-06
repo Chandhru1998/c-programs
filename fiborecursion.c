62a7a82692fddfb48a8721c24b6c5e7e6fb94ed1
Chandhru1998 <141543846+Chandhru1998@users.noreply.github.com>
2023-08-06 12:06:04 +0530

    Add files via upload

---
+++ fiborecursion.c
@@ -0,0 +1,38 @@
+/*
+Name:
+Date:
+Description:
+sample i/p:
+sample o/p:
+*/
+#include<stdio.h>
+int fibo(int f);
+int main()
+{
+    int i,n,ret,f=0;
+    printf("Enter a number: ");
+    scanf("%d",&n);
+    if(n>=0)
+    {
+	for(i=0;i<=n;i++)
+	{
+	    ret=fibo(f);
+	    printf("%d\n",ret);
+	    f++;
+	}
+    }
+    else
+    {
+	printf("Invalid input\n");
+    }
+}
+int fibo(int f)
+{
+    if(f==0)
+	return 0;
+    else if(f==1)
+	return 1;
+    else
+	return fibo(f-1)+fibo(f-2);
+}
+
